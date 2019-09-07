0dc666e0ccc567e53f7ce3c6bed21ec26c35088c
ricomonster <rmaglayon@gmail.com>
2019-09-08 00:36:39 +0800

    added CORS configuration for the GraphQL server. passed the container to the Apollo context so the resolvers can access the contents of it.

--- src/interfaces/graphql/Server.js
+++ src/interfaces/graphql/Server.js
@@ -1,34 +1,42 @@
 // dependencies
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
+const cors = require('cors');
 
 // graphql schemas
 const typeDefs = require('./type-defs');
 const resolvers = require('./resolvers');
 
 class Server {
-  constructor({ config, logger }) {
+  constructor(container) {
     this.app = express();
-    this.config = config;
-    this.logger = logger;
+    this.container = container;
   }
 
   start() {
     const { app } = this;
+    const { config, logger } = this.container;
 
     // build up the graphql server
     const server = new ApolloServer({
       typeDefs,
       resolvers,
+      context: ({ req }) => ({
+        operations: this.container,
+        ...req,
+      }),
     });
 
+    // setup cors
+    app.use(cors());
+
     // set middlewares
-    server.applyMiddleware({ app, path: this.config.graphqlEndpoint });
+    server.applyMiddleware({ app, path: config.graphqlEndpoint, cors: false });
 
     // run the server
     return new Promise(() => {
-      app.listen({ port: this.config.port }, () => {
-        this.logger.info(`GraphQL server running at http://localhost:3000${server.graphqlPath}`);
+      app.listen({ port: config.port }, () => {
+        logger.info(`GraphQL server running at http://localhost:3000${server.graphqlPath}`);
       });
     });
   }
