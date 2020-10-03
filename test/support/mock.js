26d9dde817e708c100eb164ea6e2e65e04278ee1
ricomonster <rmaglayon@gmail.com>
2020-10-03 16:55:34 +0800

    added initial test cases for app functions. added mock utilities. added nyc for code coverage

---
+++ test/support/mock.js
@@ -0,0 +1,13 @@
+module.exports = {
+  user: {
+    id: 1,
+    name: 'Name',
+    email: 'email@email.com',
+    password: '$2b$10$YaeJvodd8PIOgQ6YjqJI0.XC8DitB7xIYSUDu265efCMYZjdnfckm',
+    verifiedAt: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  jwtToken:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoibmFtZSIsImVtYWlsIjoiZW1haWxAZW1haWwuY29tIiwiaWF0IjoxNjAxNjA0MjA5LCJleHAiOjE2MDE2MDc4MDl9.zK3zRHugUXRHCdmQQZQxbSR7vxEe9YS9ArbgTasUnVM',
+};
---
+++ test/unit/app/auth/AuthorizeAuth.spec.js
@@ -0,0 +1,72 @@
+const { expect } = require('chai');
+
+const { user } = require('test/support/mock');
+
+const AuthorizeAuth = require('src/app/auth/AuthorizeAuth');
+const AuthRepository = require('src/infra/repository/AuthRepository');
+
+const AuthRepository = describe('App :: Auth :: AuthorizeAuth', () => {
+  context('should work properly', () => {
+    let authorizeAuth;
+
+    before(() => {
+      const MockUserRepository = {
+        find: (field, value) => {
+          return Promise.resolve({
+            dataValues: {
+              [field]: value,
+              ...user,
+            },
+          });
+        },
+      };
+
+      const currentTime = Math.round(Date.now() / 1000);
+      const MockAuthRepository = {
+        decodeToken: () => {
+          return {
+            id: 1,
+            name: user.name,
+            email: user.email,
+            iat: currentTime,
+            exp: currentTime + 3600,
+          };
+        },
+      };
+
+      authorizeAuth = new AuthorizeAuth({
+        authRepository: MockAuthRepository,
+        userRepository: MockUserRepository,
+      });
+    });
+
+    it('will authorize and return the user', async () => {
+      const user = await authorizeAuth.execute('sometoken');
+
+      expect(user).to.be.an('object');
+      expect(user).to.have.property('id');
+      expect(user).to.have.property('email');
+      expect(user.id).to.be.equal(1);
+      expect(user.email).to.be.equal('email@email.com');
+    });
+  });
+
+  context('invalid data provided', () => {
+    it('should fail due to token is expired', () => {
+      let authorizeAuth;
+
+      before(() => {
+        const MockUserRepository = {
+          find: (field, value) => {
+            return Promise.resolve({});
+          },
+        };
+
+        authorizeAuth = new AuthorizeAuth({
+          authRepository: new AuthRepository(),
+          userRepository: MockUserRepository,
+        });
+      });
+    });
+  });
+});
