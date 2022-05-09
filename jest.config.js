aca898eec083356789abab631c6dedab01fea66a
ricomonster <2063313+ricomonster@users.noreply.github.com>
2022-05-09 13:56:49 +0800

    updated some configurations. added npmrc

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,26 @@
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**',
+    // Do not read index.ts files
+    '!<rootDir>/src/modules/**/index.ts',
+  ],
   coveragePathIgnorePatterns: [
-    "<rootDir>/src/config/",
-    "<rootDir>/src/shared/infrastructure/database/migrations/",
-    "<rootDir>/src/shared/infrastructure/database/seeds/"
+    '<rootDir>/src/config/',
+    '<rootDir>/src/shared/infrastructure/database/migrations/',
+    '<rootDir>/src/shared/infrastructure/database/seeds/',
   ],
   coverageThreshold: {
     global: {
-      lines: 90
-    }
+      lines: 90,
+    },
   },
   moduleFileExtensions: ['js', 'json', 'ts'],
   preset: 'ts-jest',
   setupFiles: [
-    "<rootDir>/config/jest/setup.ts"
+    '<rootDir>/config/jest/setup.ts',
   ],
   testEnvironment: 'node',
   testRegex: '(/__tests__/.*(test|spec))\\.[jt]sx?$',
-  verbose: true
+  verbose: true,
 };
